7fa37525e9fdb69aada9daa96a4008a4bb36933d
nekomancer0 <nekomancers0@gmail.com>
2024-08-23 17:33:57 +0200

    Forgor to build lul

--- dist/index.js
+++ dist/index.js
@@ -1,134 +1,153 @@
-/** @extends EventTarget */
-class BoardMaker extends EventTarget {
-    width;
-    height;
-    context;
-    boxNames;
-    options;
-    /** @default undefined[] */
-    metadatas = [];
-    /**
-       * @param {Options} options
-       */
-    constructor(options) {
-        super();
-        this.width = options.width;
-        this.height = options.height;
-        this.context = options.context;
-        this.boxNames = options.boxNames;
-        this.options = options;
-    }
-    /**
-     * @description Initialize and populate the grid, don't forget to add style.
-       * @param {HTMLDivElement} [context]
-       * @returns {void}
-       */
-    init(context) {
-        if (typeof context === "undefined")
-            context = this.context;
-        context.innerHTML = "";
-        for (let r = 1; r <= this.height; r++) {
-            let col = document.createElement("div");
-            col.classList.add("col", `col-${r}`);
-            for (let i = 1; i <= this.width; i++) {
-                let item = document.createElement("div");
-                item.classList.add("item", `item-${i}`);
-                col.appendChild(item);
-                //@ts-ignore
-                item.addEventListener("click", (ev) => {
-                    this.dispatchEvent(new CustomEvent("click", { detail: { item: ev.target } }));
-                });
-                //@ts-ignore
-                item.addEventListener("mouseover", (ev) => {
-                    this.dispatchEvent(new CustomEvent("mouseover", { detail: { item: ev.target } }));
-                });
-            }
-            context.appendChild(col);
-            this.dispatchEvent(new CustomEvent("init", {
-                detail: { context: this.context || context },
-            }));
+function BoardMaker(context, options) {
+    return new (class BoardMaker {
+        width;
+        height;
+        context;
+        options;
+        events = new EventTarget();
+        borderColor = "black";
+        measure = "px";
+        borderSize = 1;
+        constructor(context, options) {
+            this.width = options.width;
+            this.height = options.height;
+            this.context = context;
         }
-    }
-    /**
-     * Useful to save the current board you just made.
-       * @returns {Metadata[]}
-       */
-    toJSON() {
-        return this.metadatas;
-    }
-    /**
-     * Convert your actual context grid board to metadata
-       * @returns {Metadata[]}
-       */
-    toMetadatas() {
-        let ret = [];
-        let cols = this.context.querySelectorAll(".col");
-        cols.forEach((col) => {
-            let items = col.querySelectorAll(".item");
-            items.forEach((item) => {
-                let position = this.getPosition(item);
-                let name = item.className.split(" ").at(2);
-                ret.push({
-                    position,
-                    //@ts-ignore
-                    name: name,
+        on(eventName, listener) {
+            this.events.addEventListener(eventName, (...args) => {
+                listener(args);
+            });
+        }
+        emit(eventName, ...args) {
+            this.events.dispatchEvent(new CustomEvent(eventName, { detail: args }));
+        }
+        /**
+         * @description Initialize and populate the grid, don't forget to add style.
+         * @param {HTMLDivElement | undefined} context
+         */
+        init() {
+            this.context.innerHTML = "";
+            for (let r = 1; r <= this.height; r++) {
+                let col = document.createElement("div");
+                col.classList.add("col", `col-${r}`);
+                for (let i = 1; i <= this.width; i++) {
+                    let item = document.createElement("div");
+                    item.classList.add("item", `item-${i}`);
+                    col.appendChild(item);
+                }
+                this.context.appendChild(col);
+            }
+            this.emit("init", this.context);
+            return this;
+        }
+        /**
+         * Convert your actual context grid board to metadata
+         * @returns {Position[]}
+         */
+        getPositions() {
+            let ret = [];
+            let cols = this.context.querySelectorAll(".col");
+            cols.forEach((col) => {
+                let items = col.querySelectorAll(".item");
+                items.forEach((item) => {
+                    let position = this.getPosition(item);
+                    ret.push(position);
                 });
             });
-        });
-        return ret;
-    }
-    /**
-       * @param {"click" | "init" | "hover"} event
-       * @param {| ((...args: any[]) => void)
-       *       | (() => void)
-       *       | ((item: HTMLDivElement) => void)} callback
-       * @returns {void}
-       */
-    on(event, callback) {
-        this.addEventListener(event, (...args) => {
-            callback(args[0]);
-        });
-    }
-    /**
-     * @description Get a box with its position, possibly to edit it, add style or get its class list.
-     * @description Only call at after .init();
-       * @param {Position} position
-       * @returns {HTMLDivElement | null}
-       */
-    //@ts-ignore
-    getElement(position) {
-        let context = this.context;
-        let col = context.querySelector(`.col-${position.x}`);
-        let item = col.querySelector(`.item-${position.y}`);
-        return item;
-    }
-    /**
-       * @param {HTMLDivElement} item
-       * @returns {Position}
-       */
-    getPosition(item) {
-        let parent = item.parentElement;
-        let yClasses = item.className.split(" ");
-        let xClasses = parent.className.split(" ");
-        let y = parseInt(yClasses[1].split("-")[1]);
-        let x = parseInt(xClasses[1].split("-")[1]);
-        return { x, y };
-    }
+            return ret;
+        }
+        /**
+         * @description Get a box with its position, possibly to edit it, add style or get its class list.
+         * @description Only call at after .init();
+         * @param position
+         * @returns {HTMLDivElement | null}
+         */
+        //@ts-ignore
+        getElement(position) {
+            let context = this.context;
+            let col = context.querySelector(`.col-${position.x}`);
+            let item = col.querySelector(`.item-${position.y}`);
+            return item;
+        }
+        getElements() {
+            let positions = this.getPositions();
+            let elements = [];
+            for (let pos of positions) {
+                let el = this.getElement(pos);
+                elements.push(el);
+            }
+            return elements;
+        }
+        _elements() {
+            let elements = this.getElements();
+            return {
+                /**
+                 * @default 1
+                 * @param num size of borders.
+                 */
+                size: (num) => {
+                    elements.forEach((element) => {
+                        element.style.border = `${num}${this.measure} solid ${this.borderColor}`;
+                    });
+                    this.borderSize = num;
+                },
+            };
+        }
+        elements = this._elements();
+        /**
+         *
+         * @param {HTMLDivElement} item
+         * @returns {Position}
+         */
+        getPosition(item) {
+            let parent = item.parentElement;
+            let yClasses = item.className.split(" ");
+            let xClasses = parent.className.split(" ");
+            let y = parseInt(yClasses[1].split("-")[1]);
+            let x = parseInt(xClasses[1].split("-")[1]);
+            return { x, y };
+        }
+        element(position) {
+            let item = this.getElement(position);
+            return {
+                /**
+                 *
+                 * @param color set color of the borders of the box
+                 */
+                borderColor: (color) => {
+                    item.style.border = `${this.borderSize}${this.measure} solid ${color}`;
+                },
+                /**
+                 *
+                 * @param color set color of the box
+                 */
+                color: (color) => {
+                    item.style.backgroundColor = `${this.borderSize}${this.measure} solid ${color}`;
+                },
+                item,
+            };
+        }
+        _borders() {
+            let elements = this.getElements();
+            return {
+                hide: () => {
+                    elements.forEach((element) => {
+                        element.style.border = "1px solid transparent";
+                    });
+                },
+                show: () => {
+                    elements.forEach((element) => {
+                        element.style.border = "1px solid black";
+                    });
+                },
+                colorize: (color) => {
+                    elements.forEach((element) => {
+                        element.style.border = "1px solid " + color;
+                    });
+                    this.borderColor = color;
+                },
+            };
+        }
+        borders = this._borders();
+    })(context, options);
 }
-/**
- * @typedef {Object} Position
- * @property {number} x
- * @property {number} y
- */
-/**
- * @typedef {Object} Metadata
- * @property {Position} position
- * @property {string} name
- */
-/**
- * @typedef {Object} Options
- * @property {number} width
- * @property {number} height
- * @property {HTMLDivElement} [context]
- * @property {string[]} [boxNames]
- */
--- readme.md
+++ readme.md
@@ -1,67 +1,206 @@
-# Boardmaker Lib
+# `BoardMaker`
 
-Create HTML checkerboard-like Board Games
+The `BoardMaker` function creates and returns an instance of a dynamic grid board within a specified `HTMLDivElement` context. This board supports various operations like initializing the grid, manipulating elements, and handling custom events.
 
-## Use on the Browser
+## Constructor
 
-**jsdelivr**
+### `BoardMaker(context: HTMLDivElement, options: Options): BoardMaker`
 
-```html
-<script src="
-https://cdn.jsdelivr.net/npm/boardmaker-lib@latest/dist/index.min.js
-"></script>
-```
+Creates a new board within a given `HTMLDivElement` context using the specified options.
 
-## Example
+#### Parameters
+
+- `context` (`HTMLDivElement`): The container element where the grid will be initialized.
+- `options` (`Options`): Configuration options for the board, which include:
+  - `width` (`number`): The number of columns in the grid.
+  - `height` (`number`): The number of rows in the grid.
+
+#### Returns
+
+- `BoardMaker`: An instance of the `BoardMaker` class with several methods and properties for managing the grid.
+
+## Properties
+
+- `width` (`number`): The width (number of columns) of the grid.
+- `height` (`number`): The height (number of rows) of the grid.
+- `context` (`HTMLDivElement`): The HTML container for the grid.
+- `elements` (`object`): Methods to manipulate the grid elements.
+- `borders` (`object`): Methods to manipulate the grid borders.
+
+## Methods
+
+### `init(): this`
+
+Initializes and populates the grid within the specified context. Clears any existing content and creates a grid based on the provided `width` and `height`.
+
+#### Returns
+
+- `this`: The current instance of `BoardMaker` for chaining purposes.
+
+#### Example
 
 ```js
-let board = new BoardMaker({
-  width: 20, // 20 boxes as width
-  height: 35, // 35 boxes as height
+board.init();
+```
+
+### `on(eventName: string, listener: (...args: any[]) => void): void`
+
+Adds an event listener for a specific event name.
+
+**Parameters**
+
+- eventName (string): The name of the event to listen for.
+- listener (function): The callback function to execute when the event is triggered.
+
+#### Example
+
+```js
+board.on("init", (context) => {
+  console.log("Board initialized!", context);
 });
-
-let item = board.getElement({ x: 1, y: 1 }); // <div class="item item-1"></div>
-
-item.style.backgroundColor = "black";
-
-let position = board.getPosition(item);
-
-console.log(position.x, position.y); // {x: 1, y: 1}
 ```
 
-## Don't forget to add style
+### `getPositions(): Position[]`
 
-**Here is the default one, please add it to one of your CSS files:**
+Converts the current grid context into metadata, returning an array of positions for each element in the grid.
 
-```css
-.context {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-}
-.context .col {
-  display: flex;
-  flex-direction: column-reverse;
-  justify-content: center;
-}
-.context .col .item {
-  border: 1px solid black;
-  width: 27px;
-  height: 27px;
-  text-align: center;
-}
+#### Returns
+
+- `Position[]`: An array of positions representing each element in the grid with x and y.
+
+#### Example
+
+```javascript
+const positions = board.getPositions();
+console.log(positions);
 ```
 
-_or, add it in your <head>_
+### `getElement(position: Position): HTMLDivElement | null`
 
-```html
-<style
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/boardmaker-lib@latest/dist/index.min.css"
-></style>
+Gets an element from the grid at a specified position. Should only be called after the grid has been initialized with .init().
+
+**Parameters**
+
+- `position` (`Position`): The position of the desired element in the grid.
+
+#### Returns
+
+- `HTMLDivElement | null`: The HTML element at the specified position, or null if not found.
+
+#### Example
+
+```javascript
+const element = board.getElement({ x: 1, y: 2 });
+console.log(element);
 ```
 
-## Example Tool
+### `getElements(): HTMLDivElement[]`
 
-![alt text](https://i.imgur.com/XEDac9U.png)
-[https://nekomancer0.github.io/GameBoard-Maker/](https://nekomancer0.github.io/GameBoard-Maker/)
+Retrieves all elements in the grid as an array of HTMLDivElement.
+
+#### Returns
+
+- `HTMLDivElement[]`: An array of all elements in the grid.
+
+#### Example
+
+```javascript
+const elements = board.getElements();
+console.log(elements);
+```
+
+### `elements.size(num: number): void`
+
+Sets the border size for all elements in the grid.
+
+**Parameters**
+
+- `num` (`number`): The size of the borders.
+  ####Example
+
+```javascript
+board.elements.size(2);
+```
+
+### `getPosition(item: HTMLDivElement): Position`
+
+Gets the position of a specific item in the grid.
+
+**Parameters**
+
+- `item` (`HTMLDivElement`): The item whose position is to be determined.
+
+#### Returns
+
+- `Position`: The position of the item within the grid with x and y.
+
+#### Example
+
+```javascript
+const position = board.getPosition(someItem);
+console.log(position);
+```
+
+### `element(position: Position): object`
+
+Gets the element at the specified position and provides methods to manipulate it.
+
+**Parameters**
+- position (Position): The position of the desired element.
+- 
+#### Returns
+- `object`: An object with methods to manipulate the element:
+- `borderColor(color: string)`: Sets the border color of the box.
+- `color(color: string)`: Sets the background color of the box.
+- `item (HTMLDivElement)`: The HTML element at the specified position.
+- 
+#### Example
+```javascript
+
+const elem = board.element({ x: 1, y: 2 });
+elem.borderColor('red');
+elem.color('blue');
+```
+### `borders.hide(): void`
+
+Hides all borders of the grid elements.
+
+#### Example
+```javascript
+
+board.borders.hide();
+```
+
+### `borders.show(): void`
+
+Shows all borders of the grid elements.
+
+#### Example
+```javascript
+board.borders.show();
+```
+
+### `borders.colorize(color: string): void`
+Sets the border color for all elements in the grid.
+
+**Parameters**
+- `color` (`string`): The color to set for the borders.
+- 
+#### Example
+```javascript
+
+board.borders.colorize('red');
+```
+
+## Example Usage
+```javascript
+
+const container = document.getElementById('grid-container');
+const options = { width: 5, height: 5 };
+const board = new BoardMaker(container, options);
+
+board.init();
+board.on('init', (context) => console.log('Grid initialized!', context));
+const elements = board.getElements();
+console.log(elements);
+```
